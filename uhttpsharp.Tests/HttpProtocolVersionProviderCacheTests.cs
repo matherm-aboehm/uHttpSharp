e55a78382e4cdc998985532ea6ee1f217acd40e7
Andreas Böhm <aboehm@matherm.de>
2025-07-17 12:19:50 +0200

    Added tests for parsing ProtocolVersion

---
+++ uhttpsharp.Tests/HttpProtocolVersionProviderCacheTests.cs
@@ -0,0 +1,67 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Net;
+
+namespace uhttpsharp.Tests
+{
+    public class HttpProtocolVersionProviderCacheTests
+    {
+        private const string ProtocolString = "Hello World";
+
+        private static IHttpProtocolVersionProvider GetTarget(IHttpProtocolVersionProvider child)
+        {
+            return new HttpProtocolVersionProviderCache(child);
+        }
+
+        [Test]
+        public void Should_Call_Child_With_Right_Parameters()
+        {
+            // Arrange
+            var mock = Substitute.For<IHttpProtocolVersionProvider>();
+            var target = GetTarget(mock);
+
+            // Act
+            target.Provide(ProtocolString);
+
+            // Assert
+            mock.Received(1).Provide(ProtocolString);
+        }
+
+        [Test]
+        public void Should_Return_Same_Child_Value()
+        {
+            // Arrange
+            Version expectedVersion = HttpVersion.Version11;
+
+            var mock = Substitute.For<IHttpProtocolVersionProvider>();
+            mock.Provide(ProtocolString).Returns(expectedVersion);
+            var target = GetTarget(mock);
+
+
+            // Act
+            var actual = target.Provide(ProtocolString);
+
+            // Assert
+            actual.ShouldBe(expectedVersion);
+        }
+
+        [Test]
+        public void Should_Cache_The_Value()
+        {
+            // Arrange
+            var mock = Substitute.For<IHttpProtocolVersionProvider>();
+            var target = GetTarget(mock);
+
+            // Act
+            target.Provide(ProtocolString);
+            target.Provide(ProtocolString);
+            target.Provide(ProtocolString);
+
+            // Assert
+            mock.Received(1).Provide(ProtocolString);
+        }
+
+    }
+}
---
+++ uhttpsharp.Tests/HttpProtocolVersionProviderTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Net;
+
+namespace uhttpsharp.Tests
+{
+    public class HttpProtocolVersionProviderTests
+    {
+        private static IHttpProtocolVersionProvider GetTarget()
+        {
+            return new HttpProtocolVersionProvider();
+        }
+
+        private static Version GetExpectedProtocolVersion(string protocol)
+        {
+            switch (protocol)
+            {
+                case "HTTP/1.0": return HttpVersion.Version10;
+                case "http/1.1": return HttpVersion.Version11;
+                case "": return new Version(0, 9);
+                case "HTTP/2": return new Version(2, 0);
+                default: return null;
+            }
+        }
+
+        [Test]
+        [TestCase("HTTP/1.0")]
+        [TestCase("http/1.1")]
+        [TestCase("")]
+        [TestCase("HTTP/2")]
+        public void Should_Succeed_And_Get_Right_Version(string protocol)
+        {
+            // Arrange
+            var target = GetTarget();
+            var expected = GetExpectedProtocolVersion(protocol);
+
+            // Act
+            var actual = target.Provide(protocol);
+
+            // Assert
+            actual.ShouldBe(expected);
+        }
+
+        [Test]
+        [ExpectedException(ExpectedException = typeof(FormatException))]
+        public void Should_Fail()
+        {
+            // Arrange
+            var target = GetTarget();
+
+            // Act
+            var actual = target.Provide("some random string");
+        }
+    }
+}
