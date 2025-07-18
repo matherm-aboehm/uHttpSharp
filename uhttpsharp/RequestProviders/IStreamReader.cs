e37cfbba0d8ef61e87e558bb778c8e0d974c6675
Andreas Böhm <aboehm@matherm.de>
2025-07-18 13:36:34 +0200

    Use safe ASCII or custom decoding for ReadLine and improve performance
    Safe ASCII decoding means it throws exception if non-ASCII chars are detected.
    Also fixes newline detection and stripping of those.

--- uhttpsharp/RequestProviders/IStreamReader.cs
+++ uhttpsharp/RequestProviders/IStreamReader.cs
@@ -47,16 +47,19 @@ namespace uhttpsharp.RequestProviders
 
     class MyStreamReader : IStreamReader
     {
+        private static readonly Encoding SafeAsciiEncoding = Encoding.GetEncoding(Encoding.ASCII.CodePage, EncoderFallback.ExceptionFallback, DecoderFallback.ExceptionFallback);
         private const int BufferSize = 8192 / 4;
         private readonly Stream _underlyingStream;
+        private readonly Encoding _encoding;
 
         private readonly byte[] _middleBuffer = new byte[BufferSize];
         private int _index;
         private int _count;
 
-        public MyStreamReader(Stream underlyingStream)
+        public MyStreamReader(Stream underlyingStream, Encoding encoding = null)
         {
             _underlyingStream = underlyingStream;
+            _encoding = encoding ?? SafeAsciiEncoding;
         }
 
         private async Task<bool> ReadBuffer()
@@ -79,30 +82,47 @@ namespace uhttpsharp.RequestProviders
 
         public async Task<string> ReadLine()
         {
-            var builder = new StringBuilder(64);
+            //HINT: Using plain StringBuilder would be faster but doesn't check for encoding errors.
+            //Using StringBuilder in combination with Encoding.GetString() for each character would be much slower
+            //than doing it only once for byte array, so instead using MemoryStream for buffer.
+            var builder = new MemoryStream(64);
 
             if (_index == _count)
             {
                 if (!await ReadBuffer().ConfigureAwait(false))
                     return string.Empty;
             }
+            int startIndex = _index;
             var readByte = _middleBuffer[_index++];
+            byte lastByte = 0;
 
-            while (readByte != '\n' && (builder.Length == 0 || builder[builder.Length - 1] != '\r'))
+            while (readByte != '\n' && lastByte != '\r')
             {
-                builder.Append((char)readByte);
+                lastByte = readByte;
 
                 if (_index == _count)
                 {
+                    builder.Write(_middleBuffer, startIndex, _count - startIndex - (lastByte == '\r' ? 1 : 0));
+                    startIndex = 0;
                     if (!await ReadBuffer().ConfigureAwait(false))
                         break;
                 }
                 readByte = _middleBuffer[_index++];
             }
+            int count = _count == 0 ? 0 : _index - 1;
 
             //Debug.WriteLine("Readline : " + sw.ElapsedMilliseconds);
 
-            return builder.ToString(0, builder.Length - 1);
+            if (lastByte == '\r' && count > 0)
+                count--;
+            if (builder.Length == 0)
+                if ((count - startIndex) == 0)
+                    return string.Empty;
+                else
+                    return _encoding.GetString(_middleBuffer, startIndex, count - startIndex);
+            else if ((count - startIndex) != 0)
+                builder.Write(_middleBuffer, startIndex, count - startIndex);
+            return _encoding.GetString(builder.ToArray());
         }
 
         public async Task<byte[]> ReadBytes(int count)
